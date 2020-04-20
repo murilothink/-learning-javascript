95fae9119c62e2840734fbb6dceac5051953f6b2
Murilo Henrique Gomes <CIANDT\murilohg@lnb024429cps.ciandt.global>
2020-04-20 14:27:34 -0300

    Adiconando o laço for

--- TabelaImc/js/principal.js
+++ TabelaImc/js/principal.js
@@ -2,64 +2,44 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Norte";
 
-
 var PesoValido = true;
 var AlturaValida = true;
 
-var paciente = document.querySelector("#primeiro");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-if(altura > 3.00 || altura < 0){
-    console.log("Altura invalida")
-    AlturaValida = false;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+    var imc = peso / (altura * altura);
+
+    if(altura > 3.00 || altura < 0){
+        console.log("Altura invalida")
+        AlturaValida = false;
+        var altura = paciente.querySelector(".info-altura");
+        altura.textContent = ("Alura Invalida");
+    }
+
+    if(peso < 0 || peso > 1000){
+        console.log("Peso invalida");
+        PesoValido = false;
+        var peso = paciente.querySelector(".info-peso");
+        peso.textContent = ("Peso Invalido");  
+    }
+
+    if(PesoValido && AlturaValida){
+        console.log(imc)
+        AlturaValida = true;
+        tdImc.textContent = imc;
+    }
+    
+    
 }
-
-if(peso < 0 || peso > 1000){
-    console.log("Peso invalida");
-    falso = confirmação;
-}
-
-if(PesoValido && AlturaValida){
-    console.log("Esta tudo OK!");
-}
-
-
-tdImc.textContent = imc;
-
-
-var pacienteTres = document.querySelector("#segundo");
-
-var tdPesoTres = pacienteTres.querySelector(".info-peso");
-var pesoTres = tdPesoTres.textContent;
-
-var tdAlturaTres = pacienteTres.querySelector(".info-altura");
-var alturaTres = tdAlturaTres.textContent;
-
-var tdImcTres = pacienteTres.querySelector(".info-imc");
-var imcTres = pesoTres / (alturaTres * alturaTres);
-
-tdImcTres.textContent = imcTres;
-
-
-var pacienteTres = document.querySelector("#terceiro");
-
-var tdPesoTres = pacienteTres.querySelector(".info-peso");
-var pesoTres = tdPesoTres.textContent;
-
-var tdAlturaTres = pacienteTres.querySelector(".info-altura");
-var alturaTres = tdAlturaTres.textContent;
-
-var tdImcTres = pacienteTres.querySelector(".info-imc");
-var imcTres = pesoTres / (alturaTres * alturaTres);
-
-tdImcTres.textContent = imcTres;
-
 
