2be745c3dbef741277dca7d35f920b32ed169af4
Murilo Henrique Gomes <CIANDT\murilohg@lnb024429cps.ciandt.global>
2020-05-01 16:54:59 -0300

    Adicionano filtro

---
+++ TabelaImc/js/filtro.js
@@ -0,0 +1,18 @@
+var campoFiltro = document.querySelector("#filtro-tabela");
+
+campoFiltro.addEventListener("input", function(){
+	console.log(this.value);
+
+	var pacientes = document.querySelectorAll(".paciente");
+
+	for (var i = 0; i < pacientes.length; i++) {
+		var paciente = pacientes[i];
+		var tdNome = paciente.querySelector(".info-nome");
+		var nome = tdNome.textContent
+		if(nome != this.value){
+			paciente.classList.add("invisivel");
+		}else{
+			paciente.classList.remove("invisivel");
+		}
+	}
+});
